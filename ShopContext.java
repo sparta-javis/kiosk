7ade3ef0e4c6484f62ebd2eb6eb1c0362553a9d7
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-06-08 15:13:18 +0900

    Update ShopContext.java

--- ShopContext.java
+++ ShopContext.java
@@ -1,106 +1,49 @@
 package team.kiosk;
 
-import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-class ShopContext {
-    private Map<String, List<Menu>> menus;
-    private Map<String, List<Item>> menuItems;
-    public List<Item> soldItems;
-    public List<Item> waitOrders;
-    public List<Item> completeOrders;
+public class ShopContext {
+    private List<String> waitOrders;
 
     public ShopContext() {
-        menus = new HashMap<>();
-        menuItems = new HashMap<>();
-        soldItems = new ArrayList<>();
         waitOrders = new ArrayList<>();
-        completeOrders = new ArrayList<>();
-
-        initializeMenuItems();
     }
 
-    private void initializeMenuItems() {
-        List<Menu> mainMenus = new ArrayList<>();
-        mainMenus.add(new Menu("Burgers", "앵거스 비프 통살을 다져만든 버거", "123"));
-//        mainMenus.add(new Menu("Forzen Custard", "매장에서 신선하게 만드는 아이스크림"));
-//        mainMenus.add(new Menu("Drinks", "매장에서 직접 만드는 음료"));
-//        mainMenus.add(new Menu("Beer", "뉴욕 브루클린 브루어리에서 양조한 맥주"));
-
-        List<Menu> orderMenus = new ArrayList<>();
-//        orderMenus.add(new Menu("Order", "장바구니를 확인 후 주문합니다."));
-//        orderMenus.add(new Menu("Cancel", "진행중인 주문을 취소합니다."));
-
-        menus.put("Main", mainMenus);
-        menus.put("Order", orderMenus);
-
-        List<Item> burgersMenus = new ArrayList<>();
-//        burgersMenus.add(new Item("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-//        burgersMenus.add(new Item("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-//        burgersMenus.add(new Item("Shroom Burger", 9.4, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거"));
-//        burgersMenus.add(new Item("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-//        burgersMenus.add(new Item("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-
-        List<Item> frozenCustardMenu = new ArrayList<>();
-//        frozenCustardMenu.add(new Item("Frozen Custard Menu Item 1", 1.4, "Frozen Custard Menu Item 1 설명"));
-//        frozenCustardMenu.add(new Item("Frozen Custard Menu Item 2", 1.0, "Frozen Custard Menu Item 2 설명"));
-//        frozenCustardMenu.add(new Item("Frozen Custard Menu Item 3", 1.6, "Frozen Custard Menu Item 3 설명"));
-//        frozenCustardMenu.add(new Item("Frozen Custard Menu Item 4", 2.1, "Frozen Custard Menu Item 4 설명"));
-
-        List<Item> drinksMenu = new ArrayList<>();
-//        drinksMenu.add(new Item("Drinks Menu Item 1", 1.0, "Drinks Menu Item 1 설명"));
-//        drinksMenu.add(new Item("Drinks Menu Item 2", 1.0, "Drinks Menu Item 2 설명"));
-
-        List<Item> beerMenu = new ArrayList<>();
-//        beerMenu.add(new Item("Beer Menu Item 1", 3.0, "Beer Menu Item 1 설명"));
-//        beerMenu.add(new Item("Beer Menu Item 2", 4.0, "Beer Menu Item 2 설명"));
-
-        menuItems.put("Burgers", burgersMenus);
-        menuItems.put("Frozen Custard", frozenCustardMenu);
-        menuItems.put("Drinks", drinksMenu);
-        menuItems.put("Beer", beerMenu);
-
-        List<Item> waitOrders = new ArrayList<>();
-        List<Item> completeOrders = new ArrayList<>();
-    }
-
-    public List<Menu> getMenus(String key) {
-        return menus.get(key);
-    }
-
-    public List<Item> getMenuItems(String key) {
-        return menuItems.get(key);
-    }
-
-
-    public void displayWaitOrder() {
-        for (Item item : waitOrders ) {
-            System.out.println(item.name + "   | " + item.price + " | " + item.description);
-        }
-    }
-
-
-    public List<Item> getWaitOrders() {
+    public List<String> getWaitOrders() {
         return waitOrders;
     }
 
+    public void addWaitOrder(int waitNumber, List<String> orderItems, double totalPrice, String request, LocalDateTime orderDateTime) {
+        String waitOrderString = "대기 번호: " + waitNumber + ""
+                + "주문 상품 목록: " + String.join(", ", orderItems) + ""
+                + "주문 총 가격: " + totalPrice + ""
+                + "요청 사항: " + request + ""
+                + "주문 일시: " + orderDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
-    public void displayCompleteOrder() {
-        List<> completeOrders = shopContext.getCompleterders();
-        for (Order order : completeOrders ) {
-            System.out.println(order.getItem().getName() + "   | " + item.price + " | " + item.description);
+        waitOrders.add(waitOrderString);
+    }
+
+    public void completeWaitOrder(int index) {
+        if (index >= 0 && index < waitOrders.size()) {
+            waitOrders.remove(index);
         }
     }
 
-    public List<Item> getCompleteOrders() {
-        return completeOrders;
+    // 대기 주문을 완료로 변경하는 메서드
+    public void waitToComplete() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("대기주문을 완료로 바꾸시겠습니까?");
+        int input = scanner.nextInt();
+        if (input == 1) {
+            System.out.println("주문이 완료되었습니다.");
+        } else if (input == 2) {
+            System.out.println("대기 중 주문입니다.");
+        } else {
+            System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+        }
     }
-
-    public void addMenuItemsInput() {
-
-    }
-
-    public static void deleteMenuItemsInput() {
-
-    }
-
 }
